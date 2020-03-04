caeba7a4e790c5ee942045482f54d9803dcca31c
Andreas Haas <andreasjhaas@gmail.com>
2020-03-04 13:15:50 +0100

    implemented darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lotto Field Rondomizer',
+      darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
