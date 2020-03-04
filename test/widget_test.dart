51a8561b8a60786fadbfea8109d0a6eb23f7431d
Andreas Haas <andreasjhaas@gmail.com>
2020-03-04 08:18:10 +0100

    changed app-name and deleted debug banner

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lotto_field_randomizer/main.dart';
+import 'package:LottoFieldRandomizer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
