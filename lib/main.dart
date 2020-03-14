d1d23ed4b596913cec6dd975bc1aef29b394b828
Andreas J. Haas <andreasjhaas@gmail.com>
2020-03-14 02:27:41 +0100

    modified reload visibility

--- lib/main.dart
+++ lib/main.dart
@@ -87,6 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
   List<LottoField> lf = [];
   TextEditingController superTC = new TextEditingController();
   String _jsonString = '{"lottozahlen":[{"zahl":"6","haeufigkeit":"100"},{"zahl":"32","haeufigkeit":"100"},{"zahl":"49","haeufigkeit":"100"},{"zahl":"38","haeufigkeit":"100"},{"zahl":"31","haeufigkeit":"100"},{"zahl":"26","haeufigkeit":"100"},{"zahl":"22","haeufigkeit":"100"},{"zahl":"33","haeufigkeit":"100"},{"zahl":"11","haeufigkeit":"100"},{"zahl":"42","haeufigkeit":"100"},{"zahl":"3","haeufigkeit":"100"},{"zahl":"43","haeufigkeit":"100"},{"zahl":"41","haeufigkeit":"100"},{"zahl":"25","haeufigkeit":"100"},{"zahl":"27","haeufigkeit":"100"},{"zahl":"36","haeufigkeit":"100"},{"zahl":"17","haeufigkeit":"100"},{"zahl":"9","haeufigkeit":"100"},{"zahl":"7","haeufigkeit":"100"},{"zahl":"29","haeufigkeit":"100"},{"zahl":"48","haeufigkeit":"100"},{"zahl":"47","haeufigkeit":"100"},{"zahl":"19","haeufigkeit":"100"},{"zahl":"4","haeufigkeit":"100"},{"zahl":"39","haeufigkeit":"100"},{"zahl":"37","haeufigkeit":"100"},{"zahl":"18","haeufigkeit":"100"},{"zahl":"1","haeufigkeit":"100"},{"zahl":"10","haeufigkeit":"100"},{"zahl":"24","haeufigkeit":"100"},{"zahl":"5","haeufigkeit":"100"},{"zahl":"2","haeufigkeit":"100"},{"zahl":"40","haeufigkeit":"100"},{"zahl":"16","haeufigkeit":"100"},{"zahl":"35","haeufigkeit":"100"},{"zahl":"34","haeufigkeit":"100"},{"zahl":"44","haeufigkeit":"100"},{"zahl":"30","haeufigkeit":"100"},{"zahl":"23","haeufigkeit":"100"},{"zahl":"46","haeufigkeit":"100"},{"zahl":"12","haeufigkeit":"100"},{"zahl":"14","haeufigkeit":"100"},{"zahl":"20","haeufigkeit":"100"},{"zahl":"15","haeufigkeit":"100"},{"zahl":"28","haeufigkeit":"100"},{"zahl":"21","haeufigkeit":"100"},{"zahl":"8","haeufigkeit":"100"},{"zahl":"45","haeufigkeit":"100"},{"zahl":"13","haeufigkeit":"100"}]}';
+  bool reloadButton = false;
 
   Future _getData() async{
     BotToast.showLoading();
@@ -102,9 +103,15 @@ class _MyHomePageState extends State<MyHomePage> {
       return response.transform(utf8.decoder).join();
     }).then((json){
       _jsonString = json;
+      setState(() {
+        reloadButton = false;
+      });
       BotToast.showText(text:"Statistics loaded");
     }).timeout(const Duration(seconds: 10)).catchError((handleError){
       print(handleError);
+      setState(() {
+        reloadButton = true;
+      });
       BotToast.showText(text:"Statistics unavailable");
     });
     BotToast.closeAllLoading();
@@ -124,7 +131,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
     sd.startListening();
 
-    _getData();
+    setState(() {
+      _getData();
+    });
 
   }
 
@@ -1168,12 +1177,15 @@ class _MyHomePageState extends State<MyHomePage> {
             )
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _getData();
-        },
-        child: Icon(Icons.autorenew),
-        backgroundColor: Colors.green,
+      floatingActionButton: Visibility(
+        visible: reloadButton,
+        child: FloatingActionButton(
+          onPressed: () {
+            _getData();
+          },
+          child: Icon(Icons.autorenew),
+          backgroundColor: Colors.green,
+        ),
       ),
     );
   }
