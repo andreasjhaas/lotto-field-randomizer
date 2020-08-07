b942f8b8b0048d5ba0ea2648e746097ddaf22994
Andreas J. Haas <andreasjhaas@gmail.com>
2020-08-07 10:12:19 +0200

    deleted statistics and modified whole algorithm

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-import 'dart:io';
 import 'dart:math';
-import 'package:LottoFieldRandomizer/statistics.dart';
 import 'package:flutter/material.dart';
 import 'package:shake/shake.dart';
 import 'package:bot_toast/bot_toast.dart';
@@ -138,58 +135,17 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   List<LottoField> lf = [];
 
   TextEditingController superTC = new TextEditingController();
-  String _jsonString = '{"lottozahlen":[{"zahl":"6","haeufigkeit":"100"},{"zahl":"32","haeufigkeit":"100"},{"zahl":"49","haeufigkeit":"100"},{"zahl":"38","haeufigkeit":"100"},{"zahl":"31","haeufigkeit":"100"},{"zahl":"26","haeufigkeit":"100"},{"zahl":"22","haeufigkeit":"100"},{"zahl":"33","haeufigkeit":"100"},{"zahl":"11","haeufigkeit":"100"},{"zahl":"42","haeufigkeit":"100"},{"zahl":"3","haeufigkeit":"100"},{"zahl":"43","haeufigkeit":"100"},{"zahl":"41","haeufigkeit":"100"},{"zahl":"25","haeufigkeit":"100"},{"zahl":"27","haeufigkeit":"100"},{"zahl":"36","haeufigkeit":"100"},{"zahl":"17","haeufigkeit":"100"},{"zahl":"9","haeufigkeit":"100"},{"zahl":"7","haeufigkeit":"100"},{"zahl":"29","haeufigkeit":"100"},{"zahl":"48","haeufigkeit":"100"},{"zahl":"47","haeufigkeit":"100"},{"zahl":"19","haeufigkeit":"100"},{"zahl":"4","haeufigkeit":"100"},{"zahl":"39","haeufigkeit":"100"},{"zahl":"37","haeufigkeit":"100"},{"zahl":"18","haeufigkeit":"100"},{"zahl":"1","haeufigkeit":"100"},{"zahl":"10","haeufigkeit":"100"},{"zahl":"24","haeufigkeit":"100"},{"zahl":"5","haeufigkeit":"100"},{"zahl":"2","haeufigkeit":"100"},{"zahl":"40","haeufigkeit":"100"},{"zahl":"16","haeufigkeit":"100"},{"zahl":"35","haeufigkeit":"100"},{"zahl":"34","haeufigkeit":"100"},{"zahl":"44","haeufigkeit":"100"},{"zahl":"30","haeufigkeit":"100"},{"zahl":"23","haeufigkeit":"100"},{"zahl":"46","haeufigkeit":"100"},{"zahl":"12","haeufigkeit":"100"},{"zahl":"14","haeufigkeit":"100"},{"zahl":"20","haeufigkeit":"100"},{"zahl":"15","haeufigkeit":"100"},{"zahl":"28","haeufigkeit":"100"},{"zahl":"21","haeufigkeit":"100"},{"zahl":"8","haeufigkeit":"100"},{"zahl":"45","haeufigkeit":"100"},{"zahl":"13","haeufigkeit":"100"}]}';
-  String _statisticText = "load statistic";
 
-  bool _isStatisticsLoaded = false;
   bool _isUIVisible = false;
 
   AnimationController controller;
   Animation<String> animation;
 
-  _callWebservice() async{
-    BotToast.showLoading();
-
-    var _url = "https://mdm.sw-aalen.de/";
-    HttpClient client = new HttpClient();
-    client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
-
-    await client.getUrl(Uri.parse(_url)).then((request){
-      request.headers.set('content-type', 'application/json');
-      return request.close();
-    }).then((response){
-      return response.transform(utf8.decoder).join();
-    }).then((json){
-      _jsonString = json;
-      _isStatisticsLoaded = true;
-      BotToast.showText(text:"Statistics loaded");
-      _statisticText = "show statistic";
-      _reloadFAB();
-
-    }).timeout(const Duration(seconds: 10)).catchError((handleError){
-      print(handleError);
-      _isStatisticsLoaded = false;
-      BotToast.showText(text:"Statistics unavailable");
-    });
-
-    BotToast.closeAllLoading();
-  }
-
-  _reloadFAB() async{
-    animation = TypewriterTween(end: _statisticText).animate(controller);
-    await controller.reverse().whenComplete(() {
-      setState(() {});
-    });
-    await controller.forward().whenComplete(() {
-      setState(() {});
-    });
-  }
 
   _loadUI() async{
     await Future.delayed(const Duration(seconds: 1), (){setState(() {
       _isUIVisible = true;
     });});
-    await Future.delayed(const Duration(seconds: 1), (){_reloadFAB();});
   }
 
   @override
@@ -204,9 +160,6 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         }
     );
     sd.startListening();
-
-    controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
-    animation = TypewriterTween(end: _statisticText).animate(controller);
 
     _loadUI();
   }
@@ -226,30 +179,16 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         }
       }
 
-      Map<String, dynamic> _json = jsonDecode(_jsonString);
-      var maxRange = 0;
-      for(var i = 0; i < _json['lottozahlen'].length; i++){
-        maxRange = maxRange + int.parse(_json['lottozahlen'][i]['haeufigkeit']);
-      }
-      print("Max Range: "+maxRange.toString());
       Random r = new Random();
-      for(int i = 0;i < 6;i++){
-        var rResult;
-        var lz;
-        do{
-          rResult = r.nextInt(maxRange)+1;
-          print("Range-Pick-Result: "+rResult.toString());
-          var range = 0;
-          for(var i = 0; i < _json['lottozahlen'].length; i++){
-            print("Zwischen "+(range+1).toString()+" und "+(range + int.parse(_json['lottozahlen'][i]['haeufigkeit'])).toString()+" entspricht Lotto Zahl "+_json['lottozahlen'][i]['zahl'].toString());
-            if(rResult > range && rResult <= (range + int.parse(_json['lottozahlen'][i]['haeufigkeit']))){
-              lz = i;
-            }
-            range = range + int.parse(_json['lottozahlen'][i]['haeufigkeit']);
-          }
-        }while(lf[int.parse(_json['lottozahlen'][lz]['zahl'])-1].isTicked());
-        lf[int.parse(_json['lottozahlen'][lz]['zahl'])-1].setTicked(true);
-      }
+      int counter = 0;
+      do{
+        int result = r.nextInt(49);
+        if(!lf[result].isTicked()){
+          lf[result].setTicked(true);
+          counter++;
+        }
+      }while(counter<6);
+
     });
   }
 
@@ -277,7 +216,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               children: <Widget>[
                 LottoFields(lf: lf),
                 Padding(
-                  padding: EdgeInsets.only(top: 10.0),
+                  padding: EdgeInsets.only(top: 15.0),
                   child: Text(
                     "Superzahl",
                     style: TextStyle(fontSize: 18),
@@ -307,29 +246,6 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               ],
             ),
           ),
-        ),
-      ),
-      floatingActionButton: AnimatedOpacity(
-        opacity: _isUIVisible ? 1.0 : 0.0,
-        duration: Duration(milliseconds: 1000),
-        child:FloatingActionButton.extended(
-          onPressed: _isStatisticsLoaded ? () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Statistics(_jsonString)),
-            );
-          } : () {
-            _callWebservice();
-          },
-          icon: Icon(_isStatisticsLoaded ? Icons.assignment : Icons.autorenew),
-          label: AnimatedBuilder(
-            animation: animation,
-            builder: (context, child) {
-              return Text('${animation.value}',
-                  style: TextStyle(fontSize: 18));
-            },
-          ),
-          tooltip: _isStatisticsLoaded ? "show statistics" : "import statistics",
         ),
       ),
     );
